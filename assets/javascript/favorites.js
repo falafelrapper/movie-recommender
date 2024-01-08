cc5412b3ffa6d6210a41442f3f66d4a5daa878df
Ankur Mehta <ankurmehta0229@gmail.com>
2024-01-08 14:26:08 -0800

    updated favorite page

--- assets/javascript/favorites.js
+++ assets/javascript/favorites.js
@@ -1,1 +1,22 @@
 //favorites script
+
+//need a button on each movie to click to favorite
+//use localStorage to save movies that have been favorites
+//pull from localStorage to get the favorited movies
+//display what was stored in localStorage
+//add a clear button to clear out localStorage
+
+var favorite = document.getElementById("localstoragelist");
+var clearButton = document.getElementById("button-is-link");
+
+function getFavoritedMovie(){
+    var myFavorite = localStorage.getItem(favorite);
+}
+
+
+//clear
+addEventListener()
+{
+    localStorage.removeItem();
+}
+
